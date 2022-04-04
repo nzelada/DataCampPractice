2ab361a227e68efe4071b4b39aa8022df7a44fbf
Nick Zelada <nzela002@fiu.edu>
2022-04-04 14:27:09 -0400

    Update sqlPractice.sql

--- sqlPractice.sql
+++ sqlPractice.sql
@@ -24,3 +24,39 @@ SELECT country_code, size,
 FROM populations
 -- Focus on 2015
 WHERE year = 2015;
+
+SELECT country_code, size,
+    CASE WHEN size > 50000000 THEN 'large'
+        WHEN size > 1000000 THEN 'medium'
+        ELSE 'small' END
+        AS popsize_group
+-- Into table
+Into pop_plus
+FROM populations
+WHERE year = 2015;
+
+-- Select all columns of pop_plus
+Select * from pop_plus;
+
+
+SELECT country_code, size,
+  CASE WHEN size > 50000000
+            THEN 'large'
+       WHEN size > 1000000
+            THEN 'medium'
+       ELSE 'small' END
+       AS popsize_group
+INTO pop_plus       
+FROM populations
+WHERE year = 2015;
+
+-- Select fields
+select name, continent, geosize_group, popsize_group
+-- From countries_plus (alias as c)
+From countries_plus as c
+  -- Join to pop_plus (alias as p)
+join pop_plus as p
+    -- Match on country code
+  on c.code = p.country_code
+-- Order the table    
+order by geosize_group asc;
